fa956ebd9dc472cb922bd6fe755d6219bf306997
Muhammad Azeez <muhammad-azeez@outlook.com>
2020-04-27 07:52:17 +0300

    clean up stuff

--- src/DevTree.BeKurdi.Demo/Program.cs
+++ src/DevTree.BeKurdi.Demo/Program.cs
@@ -15,38 +15,5 @@ namespace DevTree.BeKurdi.Demo
         {
 
         }
-
-        public static void WriteCodeForCharacterSet(string fileName, IReadOnlyList<char> characterSet)
-        {
-            var unicodeData = File.ReadAllLines("UnicodeData.txt").Select(l =>
-            {
-                var parts = l.Split(';');
-
-                return new { Code = parts[0], Name = parts[1] };
-            }).ToDictionary(c => c.Code);
-
-//            var builder = new StringBuilder();
-//            foreach (var character in characterSet)
-//            {
-//                var characterCode = ((int)character).ToString("X4");
-//                var unicodeCharacter = unicodeData[characterCode];
-
-//                builder.Append($@"
-//        /// <summary>
-//        /// <para>Common Symbols: <code>{character}</code> - <code>U+{characterCode}</code> </para>
-//        /// <para>Unicode Name: {unicodeCharacter.Name} </para>
-//        /// </summary>
-//        public const char {ProcessName(unicodeCharacter.Name)} = '\u{characterCode}';
-//");
-//            }
-
-//            File.WriteAllText(fileName, builder.ToString());
-        }
-
-        //private static TextInfo textInfo = new CultureInfo("en-US").TextInfo;
-        //private static string ProcessName(string UnicodeName)
-        //{
-        //    return textInfo.ToTitleCase(UnicodeName.ToLower()).Replace(" ", "");
-        //}
     }
 }
